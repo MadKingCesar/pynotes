30be945d87ffa2d09267115ee8d834099d923f02
MadKingCesar <113649030+MadKingCesar@users.noreply.github.com>
2022-10-28 00:06:19 +0200

    Update stringexamples.py

--- stringexamples.py
+++ stringexamples.py
@@ -7,6 +7,7 @@ print(s2 * 3) #Prints s2 3 times
 print(s2+s1)  #Prints s2 + s1
 print(len(s2))#Prints lenght of string s2
 print(s2[i])  #Prints number i in s2 (in this case, since i = 0, it prints 't')
-for x in s2:i+=1 
+for x in s2:i+=1 #Runs through string and increases i by 1
 print("there is: ",i," numbers in i")
 print(block)
+"k" in s2 #Returns false, no K in s2
