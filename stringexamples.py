edea80eaf38bf2b8fb1f266692f5dd694a08d852
MadKingCesar <113649030+MadKingCesar@users.noreply.github.com>
2022-10-28 00:03:27 +0200

    Create stringexamples.py

---
+++ stringexamples.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+i = 0
+s1 = ''
+s2 = "text"
+block = """123"""
+print(s2 * 3)
+print(s2+s1)
+print(len(s2))
+print(s2[i])
+for x in s2:i+=1
+print("there is: ",i," numbers in i")
+print(block)
