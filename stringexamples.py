277bf35897b12cc0a7645b9700f69b6b23493d49
MadKingCesar <113649030+MadKingCesar@users.noreply.github.com>
2022-10-28 00:04:37 +0200

    Update stringexamples.py

--- stringexamples.py
+++ stringexamples.py
@@ -3,10 +3,10 @@ i = 0
 s1 = ''
 s2 = "text"
 block = """123"""
-print(s2 * 3)
-print(s2+s1)
-print(len(s2))
-print(s2[i])
-for x in s2:i+=1
+print(s2 * 3) #Prints s2 3 times
+print(s2+s1)  #Prints s2 + s1
+print(len(s2))#Prints lenght of string s2
+print(s2[i])  #Prints number i in s2 (in this case, since i = 0, it prints 't')
+for x in s2:i+=1 
 print("there is: ",i," numbers in i")
 print(block)
