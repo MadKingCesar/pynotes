f325c014cc6b389ac6a18794a8d36c2529b44fbe
MadKingCesar <113649030+MadKingCesar@users.noreply.github.com>
2022-11-03 18:56:40 +0100

    Create files.py

---
+++ files.py
@@ -0,0 +1,11 @@
+#Files
+
+r = open for input
+a = open for appending in the end
+w = create and open for input
+
+output = open('path', 'w')
+s = input.read()
+s = input.read(N)
+output.write(S)
+output.close()
